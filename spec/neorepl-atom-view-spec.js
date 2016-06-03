ca821c703756ab980a9280fd5babfc2f8b35e2a5
Anton Bezrukov <hello@antonbezrukov.ru>
2016-06-03 18:17:29 +0300

    Init

---
+++ spec/neorepl-atom-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import NeoreplAtomView from '../lib/neorepl-atom-view';
+
+describe('NeoreplAtomView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
